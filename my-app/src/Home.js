5421bc65b257f93cb826cdf037ada097aee8ac54
George Haddad <hd_george@hotmail.com>
2022-02-13 17:01:52 -0500

    Created category titles #13

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,9 +1,28 @@
+import React from "react";
+import "./Home.css";
+
+
+
+
 const Home = () => {
     return (
         <div className="home">
             <h2>Home</h2>
+            <h3 className="home_categories"> 
+            Shop by category:               
+            </h3>
+            <h4 classname= "home_categoryTitle">
+               <div>Monitors</div>
+               <div>Audio</div>
+               <div>TVs</div>
+               <div>Desks</div>
+               <div>Accessories</div>
+            </h4>
         </div>
+
     );
 }
+
+
 
 export default Home;
