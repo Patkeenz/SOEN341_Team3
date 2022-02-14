a1a9e6d95395f9b16bc21cf83c901030129aae15
melisa1026 <melipan01@gmail.com>
2022-02-14 15:07:25 -0500

    #18 Add Product Stylesheet for admin interface
    Added a stylesheet for the Add Product page. Can now close issue 18.

--- my-app/src/Product.js
+++ my-app/src/Product.js
@@ -2,6 +2,7 @@ import uploadProduct from './server/product.js';
 import { useState } from "react";
 import {getStorage, ref, uploadBytes, getDownloadURL} from "firebase/storage";
 import app from "./server/index.js";
+import './addProduct.css';
 
 const Product = () => {
     const [image, setImage] = useState(null);
@@ -37,27 +38,45 @@ const Product = () => {
     const categories = ["monitor", "audio", "peripherals", "desks", "accesories"];
     return (
         <div className="product">
-        <h2>Add a Product</h2>
-    <form>
-        <input type="text" name="name" placeholder="Product Name" id="name"/>
-        <br></br>
-        <input type="text" name="price" placeholder="Product Price" id="price" />
-        <br></br>
-        <input type="text" name="description" placeholder="Product Description" id="description"/>
-        <select id="category">
-        <option value = "" selected disabled hidden>Choose a Product Category</option>
-        <option value="monitor">monitor</option>
-        <option value="audio">audio</option>
-        <option value="peripherals">peripherals</option>
-        <option value="desks">desks</option>
-        <option value="accessories">accesories</option>
-        </select> 
-        <input type="file" onChange={handleChange}/>
-        {/* Insert an image here */}       
-        <br></br>
-        <button type="submit" onClick={handleUpload}>Add Product</button>
-    </form>
-    </div>   
+          <h2 className="page-header">Add a Product</h2>
+          <form>
+            <div className="inputSpace">
+              <label className="textboxLabel" for="nameText">Product Name</label>
+              <br/>
+              <input id="nameText" className="textbox" type="text" name="name" placeholder="Product Name" id="name"/>
+              <br/>
+            </div>
+            <div className="inputSpace">
+              <label className="textboxLabel" for="priceText">Product Price</label>
+              <br/>
+              <input id="priceText" className="textbox" type="text" name="price" placeholder="Product Price" id="price" />
+              <br/>
+            </div>
+            <div className="inputSpace">
+              <label className="textboxLabel" for="descriptionText">Product Description</label>
+              <br/>
+              <input id="descriptionText" className="textbox" type="text" name="description" placeholder="Product Description" id="description"/>
+              <br/>
+            </div>
+            <div className="inputSpace">
+              <select id="category">
+              <option value = "" selected disabled hidden>Choose a Product Category</option>
+              <option value="monitor">monitor</option>
+              <option value="audio">audio</option>
+              <option value="peripherals">peripherals</option>
+              <option value="desks">desks</option>
+              <option value="accessories">accesories</option>
+              </select> 
+            </div>
+            <div className="inputSpace">
+              <input type="file" onChange={handleChange}/>
+              {/* Insert an image here */}    
+            </div> 
+            <div className="redGlowBg">
+              <button type="submit" className="button" onClick={handleUpload}>Add Product</button>
+            </div>
+          </form>
+        </div>   
         );
 }
 
