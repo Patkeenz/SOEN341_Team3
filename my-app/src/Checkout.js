d2de9def14944b45f035860106c84aa549b9c6c1
cckarabatsos <chris.karabatsos5@gmail.com>
2022-02-15 18:32:08 -0500

    Input a template for where products will be placed in checkout for user items.

--- my-app/src/Checkout.js
+++ my-app/src/Checkout.js
@@ -1,10 +1,16 @@
+import {Card, Container} from 'react-bootstrap';
+
+import './addProduct.css';
+
 const Checkout = () => {
     return (
-        <div className="checkout">
-            <h2>Checkout</h2>
-            <p>Here are your items listed below.</p>
-
-        </div>
+        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "50vh"}}>
+            <Card className=" d-flex w-100" border="danger" style= {{ maxWidth: "600px"}}>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Here are your products Below:</h2>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 }
 
