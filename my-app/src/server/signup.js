311be1eab0f420ae5e044a77d99b645857f7a4a2
melisa1026 <melipan01@gmail.com>
2022-02-16 14:49:28 -0500

    #26 The user shall provide a valid email and password.
    Validation on the password and the verified password. Made boxes turn red when field is invalid.

--- my-app/src/server/signup.js
+++ my-app/src/server/signup.js
@@ -7,6 +7,7 @@ const auth = getAuth();
 const db = getDatabase(app);
 
 async function addUser(usertype){
+
     var collection;
     if(usertype=='admin'){
         collection = 'Admins/';
@@ -30,7 +31,81 @@ async function addUser(usertype){
         const errorCode = error.code;
         const errorMessage = error.message
         alert(errorMessage);
+
+        if(errorMessage == 'Firebase: Error (auth/email-already-in-use).')
+        {
+            var emailDiv = document.getElementById('emailDiv'); 
+            emailDiv.className = "invalidInput";
+        }
     })
 }
 
-export default addUser;
+function checkForValidPassword(userType)
+{
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const digits = /[1234567890]/;
+
+    var passwordValid = true;
+
+    // get passwords from form
+    var password = document.getElementById('pass').value;
+    var validatedPass = document.getElementById('validatedPass').value;
+
+    //remove the red outline on the textspace for resubmission
+    var emailDiv = document.getElementById('emailDiv'); 
+            emailDiv.className = "";
+
+    var passwordDiv = document.getElementById('passDiv'); 
+    passwordDiv.className = "";
+
+    // make sure password is at least 8 characters and contains at least one digit
+    if(password.length < 8)
+    {
+        passwordValid = false;
+        alert("Password must contain at least 8 characters :)");
+        passwordDiv = document.getElementById('passDiv'); 
+        passwordDiv.className = "invalidInput";
+    }
+    
+    // make sure password has a special character
+    if(!specialChars.test(password))
+    {
+        passwordValid = false;
+        alert("Password must contain at least 1 special character :)");
+        passwordDiv = document.getElementById('passDiv'); 
+        passwordDiv.className = "invalidInput";
+    }
+
+    // make sure password has a digit
+    if(!digits.test(password))
+    {
+        passwordValid = false;
+        alert("Password must contain at least 1 digit :)");
+        passwordDiv = document.getElementById('passDiv'); 
+        passwordDiv.className = "invalidInput";
+    }
+
+    var validationDiv;
+
+    // make sure both passwords are the same
+    if(password !== validatedPass)
+    {
+        passwordValid = false;
+        alert("Passwords must match :)");
+        validationDiv = document.getElementById('passVerifDiv'); 
+        validationDiv.className = "invalidInput";
+    }
+    else // make sure there is no red highlight
+    {
+        validationDiv = document.getElementById('passVerifDiv'); 
+        validationDiv.className = "";
+    }
+
+
+    // make sure pass1=pass2
+
+    if(passwordValid)
+        addUser(userType);
+}
+
+export default checkForValidPassword;
