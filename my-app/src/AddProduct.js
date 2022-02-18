5be7dfd43b38af39ab80d8888611f91f5df725c1
George Haddad <hd_george@hotmail.com>
2022-02-17 22:40:53 -0500

    Mandatory fields for adding products #28

--- my-app/src/AddProduct.js
+++ my-app/src/AddProduct.js
@@ -20,26 +20,26 @@ const Product = () => {
           <h2 className="page-header">Add a Product</h2>
           <form>
             <div className="inputSpace">
-              <label className="textboxLabel" for="nameText">Product Name</label>
+              <label className="textboxLabel" for="nameText">Product Name*</label>
               <br/>
-              <input id="name" className="textbox" type="text" name="name" placeholder="Product Name"/>
-              <br/>
-            </div>
-            <div className="inputSpace">
-              <label className="textboxLabel" for="priceText">Product Price</label>
-              <br/>
-              <input id="price" className="textbox" type="text" name="price" placeholder="Product Price"/>
+              <input id="name" className="textbox" type="text" name="name" placeholder="Product Name" required/>
               <br/>
             </div>
             <div className="inputSpace">
-              <label className="textboxLabel" for="descriptionText">Product Description</label>
+              <label className="textboxLabel" for="priceText">Product Price*</label>
               <br/>
-              <input id="description" className="textbox" type="text" name="description" placeholder="Product Description"/>
+              <input id="price" className="textbox" type="text" name="price" placeholder="Product Price" required/>
               <br/>
             </div>
             <div className="inputSpace">
-              <select id="category">
-              <option value = "" selected disabled hidden>Choose a Product Category</option>
+              <label className="textboxLabel" for="descriptionText">Product Description*</label>
+              <br/>
+              <input id="description" className="textbox" type="text" name="description" placeholder="Product Description" required/>
+              <br/>
+            </div>
+            <div className="inputSpace">
+              <select id="category" required>
+              <option value = "" selected disabled hidden>Choose a Product Category* </option>
               <option value="monitor">monitor</option>
               <option value="audio">audio</option>
               <option value="peripherals">peripherals</option>
