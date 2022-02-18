5c10dd2f414f86ea10ff47809d2c9eca3de7aec8
melisa1026 <melipan01@gmail.com>
2022-02-18 15:40:38 -0500

    #19 [Feature] : As a user I would like to be able to add a product to m…
    Worked on building checkout page with js

--- my-app/src/Checkout.js
+++ my-app/src/Checkout.js
@@ -1,16 +1,13 @@
 import {Card, Container, Row, Col, Button} from 'react-bootstrap';
 import applePic from './images/blackandwhite_cart.png';
 import './checkout.css';
-import showCategory from "./server/browse.js";
-import buildProducts from "./buildCheckoutPage.js";
+import {buildCheckoutProducts} from "./buildCheckoutPage.js";
 
 var products;
 
 const Checkout = () => {
     
-    buildProducts(false).then(val=>{
-        products = val;
-    });
+    buildCheckoutProducts(false);
 
     return (
         <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "50vh"}}>
@@ -19,23 +16,25 @@ const Checkout = () => {
                     <h2 className="text-center mb-4">Here are your products Below:</h2>
                 </Card.Body>
                 {/*the following div is where items will be inserted*/}
-                <div id="checkoutItems"> 
-                    <Row className="block-example border-top border-dark">
-                        <Col xs={3} className="text-center"><img src={applePic} className="justify-content-md-center" style= {{ width:"150px"}} alt="product"/></Col>
-                        <Col xs={5}>
+                <table id="checkoutItems"> 
+                    <tr>
+                        <th>
+                            <img src={applePic} className="productImage" alt="product"/>
+                        </th>
+                        <th>
                             <h1 className="slightlyMarginedItem">Product Name</h1>
                             <br/>
                             <p>This product is a super cool product that you should definitely buy</p>
-                        </Col>
-                        <Col>
+                        </th>
+                        <th>
                             <h1 className="center">$100</h1>
-                        </Col>
-                        <Col className="text-center">
-                            <Button style={{margin: "20px"}} variant="danger">Delete</Button>
-                            <p style={{fontSize:'30px'}} className="text-center mb-4">Quntity: 200</p>
-                        </Col>
-                    </Row>
-                </div>
+                        </th>
+                        <th className="text-center">
+                            <button className="deleteButton">Delete</button>
+                            <p style={{fontSize:'30px'}} className="text-center mb-4">Q: 200</p>
+                        </th>
+                    </tr>
+                </table>
             </Card>
         </Container>
     );
--- my-app/src/buildCheckoutPage.js
+++ my-app/src/buildCheckoutPage.js
@@ -1,48 +1,67 @@
-import getProducts from "./server/checkout.js"
+import {getItems, removeItem} from "./server/cart.js"
 
-async function buildProducts(loaded) {
+
+export async function buildCheckoutProducts(loaded) {
     if(!loaded){
-        var products = await getProducts();
+        var products = await getItems();
     }
     else{
         products = loaded;
     }
     let maindiv = document.getElementById("checkoutItems");
-    for(var i=0; i<products.length; i++){
-        var product = products[i];
-        let row = document.createElement("Row");
-        row.className = "block-example border-top border-dark";
-        let imgCol = document.createElement("Col");
-        imgCol.className = "text-center";
-        //imgCol.setAttribute("xs", {3}); unsure how to do this part
-        let pic = document.createElement("img");
-        pic.setAttribute("src", products[i].link);
-        pic.className = "justify-content-md-center";
-        pic.style = { width:"150px"};
-        let infoCol = document.createElement("Col");
-        let prodName = document.createElement("h1");
-        prodName.innerHTML = product.name;
-        prodName.className = "slightlyMarginedItem";
+    maindiv.innerHTML = "";
+    if(products != null)
+    {
+        for(var i=0; i<products.length; i++)
+        {
+            var product = products[i];
+            let row = document.createElement("tr");
+            let imgCol = document.createElement("th");
+            imgCol.className = "imgCol";
+            let pic = document.createElement("img");
+            pic.setAttribute("src", products[i].link);
+            pic.className = "productImage";
+            let infoCol = document.createElement("th");
+            let prodName = document.createElement("h1");
+            prodName.innerHTML = product.name;
+            prodName.className = "slightlyMarginedItem";
+            let lineBreak = document.createElement("br");
+            let prodDescription = document.createElement("p");
+            // prodDescription.innerHTML = product.description;
+            let priceCol = document.createElement("th");
+            let prodPrice = document.createElement("h1");
+            prodPrice.innerHTML = product.price + "$";
+            let editCol = document.createElement("th");
+            let deleteButton = document.createElement("button");
+            deleteButton.className = "deleteButton";
+            deleteButton.innerHTML = "Delete";
+            deleteButton.setAttribute("id", product.spot);
+            deleteButton.onclick = function(){removeItem(this.id);
+                buildCheckoutProducts(false);};
 
-
-        maindiv.appendChild(row);
-        maindiv.appendChild(imgCol);
-        maindiv.appendChild(pic);
-        maindiv.append(infoCol);
-        maindiv.append(prodName);
-        
-        /*
-        let p1 = document.createElement("p");
-        
-        let p2 = document.createElement("p");
-        p2.innerHTML = product.price+ "$";
-        div.appendChild(pic);
-        div.appendChild(p1);
-        div.appendChild(p2);
-        maindiv.appendChild(div);
-        */
+            maindiv.appendChild(row);
+            row.appendChild(imgCol);
+            row.append(infoCol);
+            imgCol.appendChild(pic);
+            infoCol.append(prodName);
+            infoCol.append(lineBreak);
+            //infoCol.append(prodDescription);
+            row.append(priceCol);
+            priceCol.append(prodPrice);
+            row.append(editCol);
+            editCol.append(deleteButton);
+            
+            /*
+            let p1 = document.createElement("p");
+            
+            let p2 = document.createElement("p");
+            p2.innerHTML = product.price+ "$";
+            div.appendChild(pic);
+            div.appendChild(p1);
+            div.appendChild(p2);
+            maindiv.appendChild(div);
+            */
         }
+    }
     return products
   }
-
-  export default buildProducts;
