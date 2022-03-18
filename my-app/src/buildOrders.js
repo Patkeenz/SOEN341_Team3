cb0ad0d9f9284373eadfcd3b8365ca4d929649ad
melisa1026 <melipan01@gmail.com>
2022-03-17 21:04:42 -0400

    #47 orders page
    made order page tables

--- my-app/src/buildOrders.js
+++ my-app/src/buildOrders.js
@@ -5,19 +5,16 @@ import { FaCreditCard, FaTrash, FaPenSquare } from 'react-icons/fa'
 
 
 export async function buildOrders(loaded) {
-    /*
     if(!loaded){
         var orders = await getOrdersList();
     }
     else{
         var orders = loaded;
-    }*/
-
-    alert("in");
+    }
     
     // temporary
     var orders = ["order1", "order2", "order3"];
-
+    
     // empty the page
     let maindiv = document.getElementById("usercart");
     maindiv.innerHTML="";
@@ -27,7 +24,9 @@ export async function buildOrders(loaded) {
     {
         // add the date and total of the order
         let table = document.createElement("table");
+        table.className = "redBorderedLabelBorder";
         let infoRow = document.createElement("tr");
+        infoRow.className = "redBorderedLabelBorder";
         let dateColumn = document.createElement("td");
         let dateString = document.createElement("p");
         dateString.innerHTML = "October 26";
@@ -49,6 +48,7 @@ export async function buildOrders(loaded) {
         for(var j = 0; j < items.length; j++)
         {
             let itemRow = document.createElement("tr");
+            itemRow.className = "redBorderedLabelBorder";
             let imageColumn = document.createElement("td");
             let image = document.createElement("img");
             let itemNameColumn = document.createElement("td");
@@ -62,7 +62,7 @@ export async function buildOrders(loaded) {
             table.append(itemRow);
             itemRow.append(imageColumn);
             itemRow.append(itemNameColumn);
-            itemNameColumn.append(itemNameColumn.itemName);
+            itemNameColumn.append(itemName);
             itemRow.append(itemCostColumn);
             itemCostColumn.append(priceString);
         }
